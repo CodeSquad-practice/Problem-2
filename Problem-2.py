e53382327dcb9284cba532bed03ce9a85c35a6ff
2jiyong <jdragon2000@naver.com>
2024-11-21 16:17:15 +0900

    Add parse_map function
    I added parse_map function to parse map strings to stage number and
    parsed map data.

--- Problem-2.py
+++ Problem-2.py
@@ -1,3 +1,4 @@
+# map str을 저장
 input_str="""Stage 1
 #####
 #OoP#
@@ -12,4 +13,43 @@ Stage 2
  #   O  # 
  ########
 """
+
+#=====로 나눠 2 스테이지를 리스트에 저장
+stages_str=input_str.split("=====\n")
+
+# 스테이지를 포함한 str로 된 map을 넣으면 stage number와 변환하여 저장한 리스트 return
+def parse_map(map_str):
+    # 우선 줄로 나눈다.
+    map_lines=map_str.split("\n")
+    # 첫째줄은 stage num
+    stage_num=int(map_lines[0].split(' ')[1])
+    # 둘째줄부터 parse
+    map_lines=map_lines[1:]
+    # 변환 표
+    parsing_dic={' ':0 ,'O':1,'o':2,'P':3,'#':4,'=':None}
+
+    parse_map=[]
+    for elems in map_lines:
+        # split 하는 과정에서 생긴 빈 str는 제외
+        if elems=='':
+            continue
+        line=[]
+        for elem in elems:
+            # parse 하여 append
+            line.append(parsing_dic[elem])
+        parse_map.append(line)
+    
+    return stage_num,parse_map
+
+a,b=parse_map(stages_str[1])
+print(a)
+print(b)
+
+
+
+# class stage:
+#     def __init__(self,map_str):
+#         self.map_str=map_str
+#         map_lines=map_str.split("\n")
+#         self.stage_num=int(map_lines[0].split(' ')[1])
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-#Problem-2
+# Problem-2
 ## input_str을 통해 map data 관리하기
+우선 input_str에 string 저장  
+스테이지 구분자로 나눠 스테이지 2개를 stages_str 리스트에 저장
+
+## map 정보 parse 하기
+parse_map이라는 함수는 stages_str 리스트에서 스테이지 값 하나를 받음  
+parse_map은 우선 받은 string을 줄 별로 나눠, 첫째줄에서는 스테이지 number를 가져옴.  
+그리고 나머지 줄들, 즉 map string들은 parsing dictionary를 통해 변환함.  
+결국 stage number와 parsed map 리스트를 return 한다.
